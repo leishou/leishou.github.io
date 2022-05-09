92a905d744a3f6682b51df5d950a493a0f390933
leishou <1581860352@qq.com>
2022-05-09 11:43:03 +0800

    Site updated: 2022-05-09 11:43:02

---
+++ js/weather.js
@@ -0,0 +1,14 @@
+<div id="he-plugin-standard"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "layout": "1",
+    "width": 450,
+    "height": 150,
+    "background": "1",
+    "dataColor": "FFFFFF",
+    "key": "b66393de6ee14dc5945cf150e1c337b9"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/standard/static/js/he-standard-common.js?v=2.0"></script>
